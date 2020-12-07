25bbc7f284dc9045907762bcd3d96fded98ac276
Stephen Hopkinson <stephen.hopkinson@jpmorgan.com>
2020-12-07 13:43:43 +0000

    clean up code

--- day7/exercise1/index.js
+++ day7/exercise1/index.js
@@ -1,47 +1,37 @@
 const { input, helpers } = require('../../common/set-up-workplace');
 
-const bagContainsGraph = {};
 const bagIsContainedInGraph = {};
 
 input.forEach(line => {
     const [ bagNameAdjective, bagNameColour, , , ...rest ] = line.split(' ');
     const bagName = `${bagNameAdjective} ${bagNameColour}`;
     if (rest.join(' ') === 'no other bags') {
-        bagContainsGraph[bagName] = {};
         return;
     }
-    const containedBags = {};
     for (let i = 0; i < rest.length; i += 4) {
         const [ count, innerBagNameAdjective, innerBagNameColour ] = rest.slice(i, i + 4);
         const innerBagName = `${innerBagNameAdjective} ${innerBagNameColour}`;
-        containedBags[innerBagName] = parseInt(count);
         if (!bagIsContainedInGraph[innerBagName]) {
             bagIsContainedInGraph[innerBagName] = {};
         }
         bagIsContainedInGraph[innerBagName][bagName] = parseInt(count);
     }
-    bagContainsGraph[bagName] = containedBags;
-    return;
 });
 
 let allBagsChecked = [];
 const bagsLeftToCheck = [];
 let nextBag = 'shiny gold';
 do {
-    console.log('checking', nextBag);
     const node = bagIsContainedInGraph[nextBag];
-    if (!node) {
-        nextBag = bagsLeftToCheck.pop();
-        continue;
+    if (node) {
+        const bagsContainedIn = Object.keys(node);
+        bagsContainedIn.forEach(bag => {
+            if (!allBagsChecked.includes(bag)) {
+                allBagsChecked.push(bag);
+                bagsLeftToCheck.push(bag);
+            }
+        })
     }
-    const bagsContainedIn = Object.keys(node);
-    console.log(nextBag, 'is contained in ', bagsContainedIn);
-    bagsContainedIn.forEach(bag => {
-        if (!allBagsChecked.includes(bag)) {
-            allBagsChecked.push(bag);
-            bagsLeftToCheck.push(bag);
-        }
-    })
     nextBag = bagsLeftToCheck.pop();
 } while (nextBag);
 
