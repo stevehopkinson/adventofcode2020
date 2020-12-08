5fe9149bfe5fdadba3ec4d2a580f0ecb67dfaaad
Stephen Hopkinson <stephen.hopkinson@jpmorgan.com>
2020-12-08 12:44:12 +0000

    add rxjs option

--- common/set-up-workplace.js
+++ common/set-up-workplace.js
@@ -1,7 +1,9 @@
+const rxjs = require('rxjs');
 const input = require('./read-input');
 const helpers = require('./helper-functions');
 
 module.exports = {
+    helpers,
     input,
-    helpers
+    input$: rxjs.of(input)
 };
